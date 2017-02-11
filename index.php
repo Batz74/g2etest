6ef035f772217b8941d80f55274dda2064352f2f
Batz74 <kick-boxcing@mail.ru>
2017-02-11 12:09:41 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Test web-application</title>
+<link rel="stylesheet" href="style3D.css" />
+
+</head>
+<body>
+
+<div>
+  <img src="g2F.png" class="front" id = "leftimg">
+  <img src="g2B.png" class="back" id = "leftimg">
+</div>
+
+</body>
+</html>
